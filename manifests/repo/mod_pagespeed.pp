3b41786a3f656107a7e5a462cc8321fa9e768279
Ari Pringle <apringle@gmail.com>
2014-10-08 11:47:37 -0600

    add mod-pagespeed gpgkey URL

--- manifests/repo/mod_pagespeed.pp
+++ manifests/repo/mod_pagespeed.pp
@@ -6,9 +6,10 @@ class yum::repo::mod_pagespeed (
   $baseurl = 'http://dl.google.com/linux/mod-pagespeed/rpm/stable/$basearch',
 ) {
   yum::managed_yumrepo { 'mod-pagespeed':
-    descr          => 'mod-pagespeed',
-    baseurl        => $baseurl,
-    enabled        => 1,
-    gpgcheck       => 1
+    descr    => 'mod-pagespeed',
+    baseurl  => $baseurl,
+    enabled  => 1,
+    gpgcheck => 1,
+    gpgkey   => 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
   }
 }
